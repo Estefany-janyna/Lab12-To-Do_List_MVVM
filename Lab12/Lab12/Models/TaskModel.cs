a6a4586cca1e7460dfe5dd7d9e0149b4f69986a8
Estefany-janyna <estefany.taipe@tecsup.edu.pe>
2023-12-01 17:40:47 -0500

    Agregar archivos de proyecto.

---
+++ Lab12/Lab12/Models/TaskModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab12.Models
+{
+    internal class TaskModel
+    {
+        public string id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string isCompleted { get; set; }
+        
+    }
+}
---
+++ Lab12/Lab12/ViewModels/TaskViewModel.cs
@@ -0,0 +1,81 @@
+﻿using Lab12.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Lab12.ViewModels
+{
+    public class TaskViewModel : ViewModelBase
+    {
+        int title;
+        public int Title
+        {
+            get { return title; }
+            set
+            {
+                if (title != value)
+                {
+                    title = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        int description;
+        public int Description
+        {
+            get { return description; }
+            set
+            {
+                if (description != value)
+                {
+                    description = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        ObservableCollection<TaskModel> tasks;
+
+        internal List<TaskModel> TaskModels { get; }
+
+        public ObservableCollection<TaskModel> Tasks
+        {
+            get { return tasks; } 
+            set 
+            {
+                if (tasks != value)
+                {
+                    tasks = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand Save { protected set; get; }
+        public ICommand Get { protected set; get; }
+
+        private List<TaskModel> TaskModel;
+        public TaskViewModel()
+        {
+            tasks = new ObservableCollection<TaskModel>();
+            TaskModels = new List<TaskModel>();
+
+            Save = new Command(() =>
+            {
+                TaskModel task = new TaskModel();
+                task.Title = this.Title;
+                TaskModel.Add(task);
+            });
+            Get = new Command(() =>
+            {
+                Tasks = new ObservableCollection<TaskModels>;
+            });
+
+        }
+    }
+}
